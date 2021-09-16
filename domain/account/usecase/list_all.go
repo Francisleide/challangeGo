b86261bd1e51827c9acc301197ea75cfc4c99857
Francisleide <fran.almeida1990@gmail.com>
2021-09-16 17:20:22 -0300

    feat: fit logs in domains

--- domain/account/usecase/list_all.go
+++ domain/account/usecase/list_all.go
@@ -2,10 +2,11 @@ package usecase
 
 import "github.com/francisleide/ChallengeGo/domain/entities"
 
-func (c AccountUc) ListAll() []entities.Account {
+func (c AccountUc) ListAll() ([]entities.Account, error) {
 	accounts, err := c.r.ListAllAccounts()
 	if err != nil {
 		c.log.WithError(err).Errorln("failed to list accounts")
+		return []entities.Account{}, err
 	}
-	return accounts
+	return accounts, nil
 }
