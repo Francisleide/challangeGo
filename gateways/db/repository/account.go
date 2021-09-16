d89b077ee14c771373dc751574b6047c151b8ca9
Francisleide <fran.almeida1990@gmail.com>
2021-09-16 12:09:52 -0300

    feat: add logs to repositories

--- gateways/db/repository/account.go
+++ gateways/db/repository/account.go
@@ -1,29 +1,30 @@
 package repository
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/francisleide/ChallengeGo/domain/entities"
 )
 
-func (r Repository) ListAllAccounts() []entities.Account {
+func (r Repository) ListAllAccounts() ([]entities.Account, error) {
 	var accounts []entities.Account
 
 	rows, err := r.Db.Query("SELECT id, name, cpf, secret,balance, created_at from account;")
 	defer rows.Close()
-	checkError(err)
-	fmt.Println("Reading data:")
+	if err != nil {
+		r.log.WithError(err).Errorln("Failed to perform query on account table")
+		return []entities.Account{}, err
+	}
 	for rows.Next() {
 		var account entities.Account
 		err = rows.Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
 		accounts = append(accounts, account)
+		if err != nil {
+			r.log.WithError(err).Errorln("failed to read record")
+			return []entities.Account{}, err
+		}
 
 	}
-	checkError(err)
-	err = rows.Err()
-	fmt.Println("Done.")
-	return accounts
+	r.log.Info("all records have been read")
+	return accounts, nil
 }
 
 func (r Repository) FindOne(CPF string) (entities.Account, error) {
@@ -32,27 +33,40 @@ func (r Repository) FindOne(CPF string) (entities.Account, error) {
 
 	sql := "SELECT id, name, cpf, secret,balance, created_at from account where cpf=?"
 	rows, err := r.Db.Query(sql, CPF)
-	checkError(err)
+	if err != nil {
+		r.log.WithError(err).Errorln("Failed to perform query on account table")
+		return entities.Account{}, err
+	}
 	for rows.Next() {
 		var account entities.Account
 		err = rows.Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
+		if err != nil {
+			r.log.WithError(err).Errorln("failed to read record")
+			return entities.Account{}, err
+		}
 		accounts = append(accounts, account)
-		checkError(err)
+
 	}
 	if len(accounts) == 0 {
+		r.log.Errorln("no records found")
 		return entities.Account{}, err
 	}
+	r.log.Info("the account was found")
 	return accounts[0], nil
 
 }
 
 func (r Repository) UpdateBalance(ID string, balance float64) error {
 	rows, err := r.Db.Exec("UPDATE account SET balance = ? WHERE id = ?", balance, ID)
-	checkError(err)
+	if err != nil {
+		r.log.WithError(err).Errorln("failed to run update")
+	}
 	rowCount, err := rows.RowsAffected()
 	if err != nil || rowCount < 1 {
+		r.log.WithError(err).Errorln("failed to run update")
 		return err
 	}
+	r.log.Infof("id %s account balance has been updated", ID)
 	return nil
 
 }
@@ -63,9 +77,10 @@ func (r Repository) InsertAccount(account entities.Account) error {
 		account.ID, account.Name, account.CPF, account.Secret, account.Balance, account.CreatedAt)
 
 	if err != nil {
-		checkError(err)
+		r.log.WithError(err).Errorln("failed to insert a new account")
 		return err
 	}
+	r.log.Infof("new account id %s created successfully", account.ID)
 	return nil
 
 }
@@ -74,24 +89,21 @@ func (r Repository) FindByID(accountID string) (entities.Account, error) {
 	var accounts []entities.Account
 	rows, err := r.Db.Query("select * from account where id=?", accountID)
 	if err != nil {
-		checkError(err)
+		r.log.WithError(err).Errorf("failed to find account id %s", accountID)
 		return entities.Account{}, err
 	}
 	for rows.Next() {
 		var account entities.Account
 		err = rows.Scan(&account.ID, &account.Name, &account.CPF, &account.Secret, &account.Balance, &account.CreatedAt)
+		if err != nil {
+			r.log.WithError(err).Errorf("failed to find account id %s", accountID)
+		}
 		accounts = append(accounts, account)
-		checkError(err)
 	}
 	if len(accounts) == 0 {
 		return entities.Account{}, err
 	}
+	r.log.Infof("the account was found, id: %s", accountID)
 	return accounts[0], nil
 
-}
-
-func checkError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
