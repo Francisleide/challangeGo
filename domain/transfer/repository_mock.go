85e5633a92af55c45e8ae6bf286ff6382cfc661a
Francisleide <fran.almeida1990@gmail.com>
2021-09-28 17:04:07 -0300

    test: add mocked repository for unit testing to transfer

---
+++ domain/transfer/repository_mock.go
@@ -0,0 +1,27 @@
+package transfer
+
+import (
+	"github.com/francisleide/ChallengeGo/domain/entities"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockRepository struct {
+	mock.Mock
+}
+
+//implementation of repository interfaces
+
+func (mock *MockRepository) InsertTransfer(transfer entities.Transfer) (entities.Transfer, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(entities.Transfer), args.Error(1)
+}
+func (mock *MockRepository) UpdateBalance(ID string, balance float64) error {
+	args := mock.Called()
+	return args.Error(0)
+}
+func (mock *MockRepository) ListUserTransfers(CPF string) ([]entities.Transfer, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entities.Transfer), args.Error(1)
+}
