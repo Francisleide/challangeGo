8a242af5169cf8246571ee097b88ba0338e1ff58
Francisleide <fran.almeida1990@gmail.com>
2021-10-01 17:35:28 -0300

    refactor: implement repository interface in auth domain

--- domain/auth/usecase.go
+++ domain/auth/usecase.go
@@ -1,6 +1,12 @@
 package auth
 
+import "github.com/francisleide/ChallengeGo/domain/entities"
+
 type UseCase interface {
 	CreateToken(CPF string, secret string) (string, error)
-	
+	Login(CPF, secret string) error
+}
+
+type Repository interface {
+	FindOne(CPF string) (entities.Account, error)
 }
